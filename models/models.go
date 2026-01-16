d37eebd5ff51ab1b5544f60624a3ea22c75ca2f6
xiuc <ccuix@qq.com>
2026-01-16 23:59:23 +0800

    feat: add WeiboComment data model

--- models/models.go
+++ models/models.go
@@ -40,6 +40,44 @@ type WeiboUser struct {
 	SpecialFollow     bool   `json:"special_follow"`
 }
 
+type WeiboComment struct {
+	CreatedAt          string         `json:"created_at"`
+	ID                 int64          `json:"id"`
+	RootID             int64          `json:"rootid"`
+	RootIDStr          string         `json:"rootidstr"`
+	FloorNumber        int            `json:"floor_number"`
+	Text               string         `json:"text"`
+	DisableReply       int            `json:"disable_reply"`
+	RestrictOperate    int            `json:"restrictOperate"`
+	SourceAllowClick   int            `json:"source_allowclick"`
+	SourceType         int            `json:"source_type"`
+	Source             string         `json:"source"`
+	MID                string         `json:"mid"`
+	IDStr              string         `json:"idstr"`
+	Liked              bool           `json:"liked"`
+	PicNum             int            `json:"pic_num"`
+	ReadTimeType       string         `json:"readtimetype"`
+	AnalysisExtra      string         `json:"analysis_extra"`
+	CmtExt             string         `json:"cmt_ext"`
+	MatchAIPlayPicture bool           `json:"match_ai_play_picture"`
+	RID                string         `json:"rid"`
+	AllowFollow        bool           `json:"allow_follow"`
+	ItemCategory       string         `json:"item_category"`
+	DegradeType        string         `json:"degrade_type"`
+	ReportScheme       string         `json:"report_scheme"`
+	BizMarkType        int            `json:"biz_mark_type"`
+	Comments           []WeiboComment `json:"comments"`
+	HasVisible         bool           `json:"hasvisible"`
+	PreviousCursor     int            `json:"previous_cursor"`
+	NextCursor         int            `json:"next_cursor"`
+	TotalNumber        int            `json:"total_number"`
+	SinceID            int            `json:"since_id"`
+	MaxID              int            `json:"max_id"`
+	LikeCounts         int            `json:"like_counts"`
+	TextRaw            string         `json:"text_raw"`
+	IsExpand           bool           `json:"isExpand"`
+}
+
 type UserDeteil struct {
 	UID         string
 	CreateAt    string `json:"created_at"`
